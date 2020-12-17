8b76fd1dc154d3818d32081eebfcfd25324652f8
StillMortal <nik190397@mail.ru>
2020-12-18 00:39:21 +0300

    Homework9. Update.

--- homework9/tests/test_task3.py
+++ homework9/tests/test_task3.py
@@ -1,32 +1,35 @@
 import os
 from pathlib import Path
 
+import pytest
+
 from homework9.tasks.task3 import universal_file_counter
 
 
-def test_there_are_files_with_the_extension_but_there_is_no_tokenizer():
-    directory_path = Path(os.path.abspath(os.path.dirname(__file__)))
+@pytest.fixture(scope="module")
+def directory_path():
+    return Path(os.path.abspath(os.path.dirname(__file__)))
+
+
+def test_there_are_files_with_the_extension_but_there_is_no_tokenizer(directory_path):
     result = universal_file_counter(directory_path, "txt")
 
-    assert result == 10
+    assert result == 6
 
 
-def test_there_are_files_with_the_extension_but_there_is_no_tokenizer2():
-    directory_path = Path(os.path.abspath(os.path.dirname(__file__) + "../.."))
+def test_there_are_files_with_the_extension_but_there_is_no_tokenizer2(directory_path):
     result = universal_file_counter(directory_path, "py")
 
-    assert result == 241
+    assert result == 127
 
 
-def test_there_are_files_with_the_extension_but_there_is_a_tokenizer():
-    directory_path = Path(os.path.abspath(os.path.dirname(__file__)))
+def test_there_are_files_with_the_extension_but_there_is_a_tokenizer(directory_path):
     result = universal_file_counter(directory_path, "txt", str.split)
 
-    assert result == 10
+    assert result == 6
 
 
-def test_there_are_files_with_the_extension_but_there_is_a_tokenizer2():
-    directory_path = Path(os.path.abspath(os.path.dirname(__file__)))
+def test_there_are_files_with_the_extension_but_there_is_a_tokenizer2(directory_path):
     result = universal_file_counter(directory_path, "py", str.split)
 
-    assert result == 199
+    assert result == 244
