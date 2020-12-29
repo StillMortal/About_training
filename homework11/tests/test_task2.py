b0607a7e853951cf0333c7f47725a5b1236e0c69
StillMortal <nik190397@mail.ru>
2020-12-30 00:32:01 +0300

    Homework11. Update.

--- homework11/tests/test_task2.py
+++ homework11/tests/test_task2.py
@@ -1,19 +1,19 @@
-from homework11.tasks.task2 import Order
+from homework11.tasks.task2 import ElderDiscount, MorningDiscount, NoDiscount, Order
 
 
 def test_order_without_discount():
-    order = Order(100)
+    order = Order(100, NoDiscount)
 
     assert order.final_price() == 100
 
 
 def test_order_with_a_morning_discount():
-    order = Order(100, "morning_discount")
+    order = Order(100, MorningDiscount)
 
     assert order.final_price() == 50
 
 
 def test_order_with_a_elder_discount():
-    order = Order(100, "elder_discount")
+    order = Order(100, ElderDiscount)
 
     assert order.final_price() == 10
